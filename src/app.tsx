2d336c5c1372457c3b33eef45c114c787ce00eb5
Jesus Liang <jesusliang96@gmail.com>
2022-07-22 11:12:34 +0200

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'src/app.scss'
+interface Props {
+
+}
+const app:React.FC<Props> = (props) => {
+  return (
+    <div>
+    <h1>Hello World!</h1>
+    </div>
+  )
+}
+export default app
