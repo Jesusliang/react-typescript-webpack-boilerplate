49088c6bc0f828b625fd14cd026213b5f72243d4
Jesus Liang <jesusliang96@gmail.com>
2022-11-05 11:36:09 +0100

    updated yarn version

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
-import '@testing-library/jest-dom'
 import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom'
 import { server } from './src/mocks/server'
 
 beforeAll(() => server.listen())
